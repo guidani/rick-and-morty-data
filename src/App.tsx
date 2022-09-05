36a41610bb30bba592cad3840595a478a27542e7
guilherme daniel <gui.dani13@gmail.com>
2022-09-05 10:53:53 -0300

    adicionado caixa de pesquisa

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import { Box, Center, Container, Heading, HStack, Input, VStack, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Heading,
+  HStack,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+import { FiSearch } from "react-icons/fi";
 
 function App() {
   return (
     <VStack>
-    <Box w="full" bg="brand.500">
-      <Container h="200px" centerContent>
-        <Center h="full">
-          <Heading as="h1" size="3xl" color="#fff">Rick and Morty</Heading>
-        </Center>
-      </Container>
-    </Box>
-    {/* Search box */}
-    <Box w='full'>
-      <Container>
-      <HStack>
-        <Input width="auto" htmlSize={4}/>
-        <Button colorScheme="brand.500">Pesquisar</Button>
-      </HStack>
-      </Container>
-    </Box>
+      <Box w="100%" bg="brand.500">
+        <Container h={["100px", "200px"]} maxW="container.lg">
+          <Center h="full" w="100%">
+            <Heading as="h1" size="3xl" color="#fff">
+              Rick and Morty
+            </Heading>
+          </Center>
+        </Container>
+      </Box>
+      {/* Search box */}
+      <Box w="100%">
+        <Container maxW="container.lg">
+          <HStack>
+            <Input
+              width="100%"
+              htmlSize={4}
+              placeholder="Buscar um personagem"
+            />
+            <Button leftIcon={<FiSearch />} variant="solid">
+              Pesquisar
+            </Button>
+          </HStack>
+        </Container>
+      </Box>
     </VStack>
   );
 }
