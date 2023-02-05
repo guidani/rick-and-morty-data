950dd9376e502a24c4249b89a709ba9abecddb31
guilherme daniel <gui.dani13@gmail.com>
2023-02-05 15:52:02 -0300

    grid para localizações e episodios

---
+++ src/components/EpisodesGrid.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EpisodesGrid = () => {
+  return (
+    <div>EpisodesGrid</div>
+  )
+}
---
+++ src/components/LocationsGrid.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LocationsGrid = () => {
+  return (
+    <div>LocationsGrid</div>
+  )
+}
