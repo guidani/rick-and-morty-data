09f4f53f9674e9b5f4d8989d0cf1758afcbf9541
guilherme daniel <gui.dani13@gmail.com>
2023-02-05 15:58:39 -0300

    finished - episodes grid

--- src/components/EpisodesGrid.tsx
+++ src/components/EpisodesGrid.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import { useQuery } from "@apollo/client";
+import { Box, Button, HStack, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { GET_EPISODES } from "../services/graphql/queries/getEpisodes";
+import { IEpisode } from "./IEpisode";
+import { LoadingIcon } from "./LoadingIcon";
 
 export const EpisodesGrid = () => {
+  const [counter, setCounter] = useState(1);
+  const { loading, error, data } = useQuery(GET_EPISODES, {
+    variables: { page: counter },
+  });
+
+  let hasPrev = data?.episodes?.info?.prev;
+  let hasNext = data?.episodes?.info?.next;
+
+  if (loading) return <LoadingIcon />;
+  if (error) return <p>Error...</p>;
   return (
-    <div>EpisodesGrid</div>
-  )
-}
+    <>
+      <HStack>
+        <Button
+          onClick={() => setCounter(counter - 1)}
+          disabled={hasPrev == null ? true : false}
+        >
+          Anterior
+        </Button>
+        <Button
+          onClick={() => setCounter(counter + 1)}
+          disabled={hasNext ? false : true}
+        >
+          Próximo
+        </Button>
+      </HStack>
+
+      {data?.episodes?.results.map(
+        ({ id, name, episode, air_date }: IEpisode) => (
+          <Box>
+            <Text>{id}</Text>
+            <Text>{name}</Text>
+            <Text>{episode}</Text>
+            <Text>{air_date}</Text>
+          </Box>
+        )
+      )}
+    </>
+  );
+};
--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -1,51 +1,13 @@
-import { useQuery } from "@apollo/client";
-import { Box, Button, Heading, HStack, Text } from "@chakra-ui/react";
-import { useState } from "react";
-import { IEpisode } from "../components/IEpisode";
-import { LoadingIcon } from "../components/LoadingIcon";
-import { GET_EPISODES } from "../services/graphql/queries/getEpisodes";
+import { Container, Heading } from "@chakra-ui/react";
+import { EpisodesGrid } from "../components/EpisodesGrid";
 
 export const Episodes = () => {
-  const [counter, setCounter] = useState(1);
-  const { loading, error, data } = useQuery(GET_EPISODES, {
-    variables: { page: counter },
-  });
-
-  let hasPrev = data?.episodes?.info?.prev;
-  let hasNext = data?.episodes?.info?.next;
-
-  if (loading) return <LoadingIcon />;
-  if (error) return <p>Error...</p>;
-
   return (
     <>
-      <Heading as="h2">Episodes</Heading>
-      {/*  */}
-      <HStack>
-        <Button
-          onClick={() => setCounter(counter - 1)}
-          disabled={hasPrev == null ? true : false}
-        >
-          Anterior
-        </Button>
-        <Button
-          onClick={() => setCounter(counter + 1)}
-          disabled={hasNext ? false : true}
-        >
-          Próximo
-        </Button>
-      </HStack>
-
-      {data?.episodes?.results.map(
-        ({ id, name, episode, air_date }: IEpisode) => (
-          <Box>
-            <Text>{id}</Text>
-            <Text>{name}</Text>
-            <Text>{episode}</Text>
-            <Text>{air_date}</Text>
-          </Box>
-        )
-      )}
+      <Container maxW="container.lg">
+        <Heading as="h2">Episodes</Heading>
+        <EpisodesGrid />
+      </Container>
     </>
   );
 };
