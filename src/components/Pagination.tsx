160ae8a0b08cd1c0cd20f7d32764da9086b7b893
guilherme daniel <gui.dani13@gmail.com>
2023-02-05 17:59:47 -0300

    test - tipagem paginação

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,18 +1,30 @@
 import { Button, HStack } from "@chakra-ui/react";
 
-export const Pagination = ({ onClick, data, ...rest }) => {
+type InfoProps = {
+  count: number;
+  pages: number;
+  next: number | null;
+  prev: number | null;
+}
+
+type Props = {
+  onClick: any;
+  data: InfoProps
+}
+
+export const Pagination = ({ onClick, data}: Props) => {
   let hasPrev = data.prev;
   let hasNext = data.next;
   return (
     <HStack>
       <Button
-        onClick={() => onClick((prev) => prev - 1)}
+        onClick={() => onClick((prev: number) => prev - 1)}
         disabled={hasPrev == null ? true : false}
       >
         Anterior
       </Button>
       <Button
-        onClick={() => onClick((prev) => prev + 1)}
+        onClick={() => onClick((prev: number) => prev + 1)}
         disabled={hasNext ? false : true}
       >
         Próximo
