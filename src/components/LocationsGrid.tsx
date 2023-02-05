d749975c632f9a4022014267c970cff7350fd8b2
guilherme daniel <gui.dani13@gmail.com>
2023-02-05 15:56:25 -0300

    finished - locations grid

--- src/components/LocationsGrid.tsx
+++ src/components/LocationsGrid.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import { useQuery } from "@apollo/client";
+import { Box, Button, HStack, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { GET_LOCATIONS } from "../services/graphql/queries/getLocations";
+import { ILocation } from "./ILocation";
+import { LoadingIcon } from "./LoadingIcon";
 
 export const LocationsGrid = () => {
+  const [counter, setCounter] = useState(1);
+  const { loading, error, data } = useQuery(GET_LOCATIONS, {
+    variables: { page: counter },
+  });
+
+  let hasPrev = data?.locations?.info?.prev;
+  let hasNext = data?.locations?.info?.next;
+
+  if (loading) return <LoadingIcon />;
+  if (error) return <p>Error...</p>;
   return (
-    <div>LocationsGrid</div>
-  )
-}
+    <>
+      <HStack>
+        <Button
+          onClick={() => setCounter(counter - 1)}
+          disabled={hasPrev == null ? true : false}
+        >
+          Anterior
+        </Button>
+        <Button
+          onClick={() => setCounter(counter + 1)}
+          disabled={hasNext ? false : true}
+        >
+          Próximo
+        </Button>
+      </HStack>
+      {data?.locations?.results.map(
+        ({ id, name, type, dimension }: ILocation) => (
+          <Box>
+            <Text>{id}</Text>
+            <Text>{name}</Text>
+            <Text>{type}</Text>
+            <Text>{dimension}</Text>
+          </Box>
+        )
+      )}
+    </>
+  );
+};
--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,51 +1,13 @@
-import { useQuery } from "@apollo/client";
-import { Box, Button, Heading, HStack, Text } from "@chakra-ui/react";
-import { useState } from "react";
-import { ILocation } from "../components/ILocation";
-import { LoadingIcon } from "../components/LoadingIcon";
-import { GET_LOCATIONS } from "../services/graphql/queries/getLocations";
+import { Container, Heading } from "@chakra-ui/react";
+import { LocationsGrid } from "../components/LocationsGrid";
 
 export const Locations = () => {
-  const [counter, setCounter] = useState(1);
-  const { loading, error, data } = useQuery(GET_LOCATIONS, {
-    variables: { page: counter },
-  });
-
-  let hasPrev = data?.locations?.info?.prev;
-  let hasNext = data?.locations?.info?.next;
-
-  if (loading) return <LoadingIcon />;
-  if (error) return <p>Error...</p>;
-
   return (
     <>
-      <Heading as="h2">Lugares</Heading>
-      {/*  */}
-      <HStack>
-        <Button
-          onClick={() => setCounter(counter - 1)}
-          disabled={hasPrev == null ? true : false}
-        >
-          Anterior
-        </Button>
-        <Button
-          onClick={() => setCounter(counter + 1)}
-          disabled={hasNext ? false : true}
-        >
-          Próximo
-        </Button>
-      </HStack>
-      ;{/*  */}
-      {data?.locations?.results.map(
-        ({ id, name, type, dimension }: ILocation) => (
-          <Box>
-            <Text>{id}</Text>
-            <Text>{name}</Text>
-            <Text>{type}</Text>
-            <Text>{dimension}</Text>
-          </Box>
-        )
-      )}
+      <Container maxW="container.lg">
+        <Heading as="h2">Lugares</Heading>
+        <LocationsGrid />
+      </Container>
     </>
   );
 };
