7a8a82dccaa36d9af7b3fad4260618b5e299f529
guilherme daniel <gui.dani13@gmail.com>
2023-02-08 17:49:33 -0300

    update

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Center, Container, Heading } from "@chakra-ui/react";
 
 export const Statistics = () => {
   return (
-    <div>Statistics</div>
-  )
-}
+    <>
+      <Container maxW="container.lg">
+        <Center>
+          <Heading as="h1" marginBottom="4" size="xl">
+            Estatisticas
+          </Heading>
+        </Center>
+      </Container>
+    </>
+  );
+};
