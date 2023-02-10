2d7cd545570b5a2a63f6f64f2e40f64cfcf32de3
guilherme daniel <gui.dani13@gmail.com>
2023-02-10 11:46:54 -0300

    finalizado

--- src/components/LargeNavigation.tsx
+++ src/components/LargeNavigation.tsx
@@ -1,57 +1,87 @@
-import { Center, Container, Wrap, WrapItem } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Container, Grid, GridItem, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 
 export const LargeNavigation = () => {
   return (
-    <Container maxW="container.lg" display={{ base: "none", md: "block" }}>
-      <Center my="4">
-        <Wrap w="100%">
-          <WrapItem flex="1">
-            <Center
-              w="100%"
-              minW="16rem"
-              h="40px"
-              bg="red.200"
-              borderRadius="md"
-            >
-              <Link to="/">Personagens</Link>
-            </Center>
-          </WrapItem>
-          <WrapItem flex="1">
-            <Center
-              w="100%"
-              minW="16rem"
-              h="40px"
-              bg="tomato"
-              borderRadius="md"
-            >
-              <Link to="/locations">Lugares</Link>
-            </Center>
-          </WrapItem>
-          <WrapItem flex="1">
-            <Center
-              w="100%"
-              minW="16rem"
-              h="40px"
-              bg="blue.200"
-              borderRadius="md"
-            >
-              <Link to="/episodes">Episódios</Link>
-            </Center>
-          </WrapItem>
-          <WrapItem flex="1">
-            <Center
-              w="100%"
-              minW="16rem"
-              h="40px"
-              bg="green.200"
-              borderRadius="md"
-            >
-              <Link to="/statistics">Estatisticas</Link>
-            </Center>
-          </WrapItem>
-        </Wrap>
-      </Center>
+    <Container
+      maxW="container.lg"
+      display={{ base: "none", md: "block" }}
+      marginTop="2"
+    >
+      <Grid w="100%" gridTemplateColumns="repeat(4, 1fr)" gap="5">
+        <GridItem
+          w="100%"
+          bgColor="red.400"
+          textAlign="center"
+          py="2"
+          borderRadius="md"
+          _hover={{backgroundColor: 'red.500'}}
+        >
+          <Link
+            as={RouterLink}
+            to="/"
+            display="block"
+            fontWeight="400"
+            _hover={{ textDecoration: "none" }}
+          >
+            Personagens
+          </Link>
+        </GridItem>
+        <GridItem
+          w="100%"
+          bgColor="green.400"
+          textAlign="center"
+          py="2"
+          borderRadius="md"
+          _hover={{backgroundColor: 'green.500'}}
+        >
+          <Link
+            as={RouterLink}
+            to="/locations"
+            display="block"
+            fontWeight="400"
+            _hover={{ textDecoration: "none" }}
+          >
+            Lugares
+          </Link>
+        </GridItem>
+        <GridItem
+          w="100%"
+          bgColor="blue.400"
+          textAlign="center"
+          py="2"
+          borderRadius="md"
+          _hover={{backgroundColor: 'blue.500'}}
+        >
+          <Link
+            as={RouterLink}
+            to="/episodes"
+            display="block"
+            fontWeight="400"
+            _hover={{ textDecoration: "none" }}
+          >
+            Episódios
+          </Link>
+        </GridItem>
+        <GridItem
+          w="100%"
+          bgColor="yellow.300"
+          textAlign="center"
+          py="2"
+          borderRadius="md"
+          _hover={{backgroundColor: 'yellow.500'}}
+        >
+          <Link
+            as={RouterLink}
+            to="/statistics"
+            display="block"
+            fontWeight="400"
+            _hover={{ textDecoration: "none" }}
+          >
+            Estatisticas
+          </Link>
+        </GridItem>
+      </Grid>
     </Container>
   );
 };
