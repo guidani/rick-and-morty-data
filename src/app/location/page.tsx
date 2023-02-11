3e64bc24aafe8dd8953cc6e291434442ef0b891b
guilherme daniel <gui.dani13@gmail.com>
2023-02-11 18:26:13 -0300

    realizado busca de dados

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,9 +1,40 @@
+import { useQuery } from "@apollo/client";
+import { Box, Center, Container, Flex, Heading, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import { GET_LOCATION } from "../../services/graphql/queries/getLocation";
 
 const Location = () => {
   const { id } = useParams();
+  const { data, loading, error } = useQuery(GET_LOCATION, {
+    variables: { id: id },
+  });
 
-  return <>Location {id}</>;
+  return (
+    <>
+      {console.log(data?.location)}
+      <Container maxW="container.lg">
+        <Center>
+          <Heading as="h1" marginBottom="4" size="xl">
+            Localização
+          </Heading>
+        </Center>
+
+        <Flex justify='space-between'>
+          <Text>Nome: </Text>
+          <Text>{data?.location.name}</Text>
+        </Flex>
+        <Flex justify='space-between'>
+          <Text>Dimensão: </Text>
+          <Text>{data?.location.dimension}</Text>
+        </Flex>
+        <Flex justify='space-between'>
+          <Text>Tipo: </Text>
+          <Text>{data?.location.type}</Text>
+        </Flex>
+        
+      </Container>
+    </>
+  );
 };
 
-export default Location
+export default Location;
