9cc331b6efa7a754e3588b10543b309d3d00badb
guilherme daniel <gui.dani13@gmail.com>
2023-02-11 21:18:07 -0300

    update-criado componente específico para a localização

---
+++ src/app/location/locationGrid.tsx
@@ -0,0 +1,70 @@
+import { useQuery } from "@apollo/client";
+import {
+  Container,
+  Flex,
+  Spacer,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import { ErrorWidget } from "../../components/UI/ErrorWidget";
+import { TableSkeleton } from "../../components/UI/TableSkeleton";
+import { GET_LOCATION } from "../../services/graphql/queries/getLocation";
+import { IResident } from "./IResident";
+
+export const LocationGrid = () => {
+  const { id } = useParams();
+  const { data, loading, error } = useQuery(GET_LOCATION, {
+    variables: { id: id },
+  });
+
+  if (loading)
+    return (
+      <Container maxW="container.lg">
+        <TableSkeleton />
+      </Container>
+    );
+  if (error) return <ErrorWidget />;
+  return (
+    <>
+      <Flex justify="space-between">
+        <Text>Nome: </Text>
+        <Text>{data?.location.name}</Text>
+      </Flex>
+      <Flex justify="space-between">
+        <Text>Dimensão: </Text>
+        <Text>{data?.location.dimension}</Text>
+      </Flex>
+      <Flex justify="space-between">
+        <Text>Tipo: </Text>
+        <Text>{data?.location.type}</Text>
+      </Flex>
+      <Spacer h="4" />
+      <h2>Habitantes</h2>
+      <TableContainer>
+        <Table variant="striped">
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Nome</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data?.location?.residents.map(({ id, name }: IResident) => (
+              <Tr>
+                <Td>{id}</Td>
+                <Td>{name}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,71 +1,16 @@
-import { useQuery } from "@apollo/client";
-import {
-  Center,
-  Container,
-  Flex,
-  Heading,
-  Spacer,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Text,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
-import { GET_LOCATION } from "../../services/graphql/queries/getLocation";
-import { IResident } from "./IResident";
+import { Center, Container, Heading } from "@chakra-ui/react";
+import { LocationGrid } from "./locationGrid";
 
 const Location = () => {
-  const { id } = useParams();
-  const { data, loading, error } = useQuery(GET_LOCATION, {
-    variables: { id: id },
-  });
-
   return (
     <>
-      {console.log(data?.location)}
       <Container maxW="container.lg">
         <Center>
           <Heading as="h1" marginBottom="4" size="xl">
             Localização
           </Heading>
         </Center>
-
-        <Flex justify="space-between">
-          <Text>Nome: </Text>
-          <Text>{data?.location.name}</Text>
-        </Flex>
-        <Flex justify="space-between">
-          <Text>Dimensão: </Text>
-          <Text>{data?.location.dimension}</Text>
-        </Flex>
-        <Flex justify="space-between">
-          <Text>Tipo: </Text>
-          <Text>{data?.location.type}</Text>
-        </Flex>
-        <Spacer h="4"/>
-        <h2>Habitantes</h2>
-        <TableContainer>
-          <Table variant="striped">
-            <Thead>
-              <Tr>
-                <Th>ID</Th>
-                <Th>Nome</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {data?.location?.residents.map(({ id, name }: IResident) => (
-                <Tr>
-                  <Td>{id}</Td>
-                  <Td>{name}</Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        </TableContainer>
+        <LocationGrid />
       </Container>
     </>
   );
